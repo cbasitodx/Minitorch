1a401cd7418190ff98545456f282948950e2ba50
cbasitodx <sebastiankayconde@gmail.com>
2024-08-02 17:01:26 +0200

    Added minitorch.nn.Sequential pydoc

--- minitorch/nn/Sequential.py
+++ minitorch/nn/Sequential.py
@@ -6,6 +6,57 @@ from typing import Tuple, List
 
 class Sequential(Module):
 
+    """
+    ===========
+    **Summary**
+    ===========
+
+        A sequential container.
+
+        Modules will be added to it in the order they are passed in the constructor.
+
+        The value a Sequential provides over manually calling a sequence of modules is that it allows treating the whole container as a single module.
+
+       Layers in a Sequential are connected in a cascading way.
+
+    ==============
+    **Parameters**
+    ==============
+    
+        * **__layers:** (*Tuple[callable]*) Tuple of all the callable objects passed to the Sequential.
+        * **__trainable:** (*List[callable]*) List of all the callable objects passed to the Sequential that are *trainable* (this meaning, that subclass :class:`minitorch.nn.Module`).
+
+    ======================
+    **Instance Variables**
+    ======================
+
+        * ***layers:** (*Tuple[callable]*) Tuple of all the callable objects that comprise a Sequential instance.
+
+    ===========
+    **Example**
+    ===========
+
+        .. code-block:: python
+        
+            class MLP(Module):
+                def __init__(self):
+                    self.layers = Sequential(
+                        Linear(2,3),
+                        ReLU(),
+                        Linear(3,1),
+                        Sigmoid()
+                    )
+                
+                def forward(self, input : List[float]):
+                    
+                    # We convert the input to a tensor
+                    input = Tensor(input)
+            
+                    # Now we do the forward pass
+                    return self.layers(input)
+
+    """
+
     def __init__(self, *layers):
 
         self.__layers : Tuple[callable] = layers
